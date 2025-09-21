5a4afd9b070edf43533a1510dea9dd0f13cdf31c
engmoha1717 <engmoha1717@gmail.com>
2025-09-21 02:11:28 -0700

    first init is ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 
 export default function Home() {
-
-  console.log("Clerk publishable key:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
   return (
-    <div>
-      hello
+    <div className="">
+      EventList 
     </div>
   );
 }
